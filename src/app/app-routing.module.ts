60e89c78b03b731f319fd419c6552adb29f995d0
Kareem Elsayed <kelsayed.c@saip.gov.sa>
2023-06-15 21:44:00 +0300

    utils functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { View1Component } from './shared/views/view1/view1.component';
+import { View2Component } from './shared/views/view2/view2.component';
+
+const routes: Routes = [
+  {
+    path:'view1',
+    component:View1Component,
+    data:{
+      title:'view1'
+    }
+  },
+  {
+    path:'view2',
+    component:View2Component,
+    data:{
+      title:'view2'
+    }
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { TitleTranslationProviderService } from './modules/helpers/title-translation-provider.service';
+import { WrapTranslateComponent } from './shared/components/wrap-translate/wrap-translate.component';
+import { translateConfigModule } from './core/Translate.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    translateConfigModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [TitleTranslationProviderService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (myService: TitleTranslationProviderService) => () => TitleTranslationProviderService,
+      deps: [TitleTranslationProviderService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
